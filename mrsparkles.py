ea02381188409ee895658c895dac00a617e5a70a
schobnat000 <natalie.rae.schober@gmail.com>
2016-05-06 22:39:06 -0700

    Update mrsparkles.py

--- mrsparkles.py
+++ mrsparkles.py
@@ -2,6 +2,7 @@ import turtle
 
 mrsparkles = turtle.Turtle()
 mrsparkles.shape('turtle')
+mrsparkles.speed(20)
 
 mrsparkles.pendown()
 mrsparkles.begin_fill()
@@ -20,12 +21,36 @@ mrsparkles.color('gold')
 mrsparkles.forward(100)
 mrsparkles.right(120)
 mrsparkles.forward(100)
+mrsparkles.right(120)
+mrsparkles.forward(100)
+
+mrsparkles.end_fill()
+mrsparkles.begin_fill()
+mrsparkles.color("green")
+
+mrsparkles.right(180)
 mrsparkles.forward(100)
 mrsparkles.right(120)
 mrsparkles.forward(100)
 
 mrsparkles.right(120)
 mrsparkles.forward(100)
-mrsparkles.right(120)
+
+mrsparkles.end_fill()
+
 mrsparkles.right(120)
 mrsparkles.forward(100)
+mrsparkles.right(120)
+mrsparkles.forward(100)
+
+mrsparkles.begin_fill()
+mrsparkles.color('gold')
+
+mrsparkles.left(120)
+mrsparkles.forward(100)
+mrsparkles.left(120)
+mrsparkles.forward(100)
+mrsparkles.right(180)
+mrsparkles.pendown()
+
+mrsparkles.end_fill()
