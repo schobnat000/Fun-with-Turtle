9031f0adab88abf6aaeb9546adab30f6bb0c3c29
schobnat000 <natalie.rae.schober@gmail.com>
2016-05-06 18:19:30 -0700

    Create mrsparkles.py

---
+++ mrsparkles.py
@@ -0,0 +1,31 @@
+import turtle
+
+mrsparkles = turtle.Turtle()
+mrsparkles.shape('turtle')
+
+mrsparkles.pendown()
+mrsparkles.begin_fill()
+mrsparkles.color('gold')
+
+mrsparkles.forward(100)
+mrsparkles.right(120)
+mrsparkles.forward(100)
+mrsparkles.right(120)
+mrsparkles.forward(100)
+
+mrsparkles.end_fill()
+mrsparkles.begin_fill()
+mrsparkles.color('gold')
+
+mrsparkles.forward(100)
+mrsparkles.right(120)
+mrsparkles.forward(100)
+mrsparkles.forward(100)
+mrsparkles.right(120)
+mrsparkles.forward(100)
+
+mrsparkles.right(120)
+mrsparkles.forward(100)
+mrsparkles.right(120)
+mrsparkles.right(120)
+mrsparkles.forward(100)
